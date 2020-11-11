8236d5a3cdb64c6ed1e884457f8f36d8a7ae6639
Carla Jove <cjove@edrans.com>
2020-11-10 21:39:17 -0300

    3.6 minuto 6:47

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,16 @@
 import boto3
+import sys 
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
+def list_instances():
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == '__main__':
+    print(sys.argv)
+    list_instances()
+    
+
     
